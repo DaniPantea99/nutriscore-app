3674e778fdfcad2c08ad99556f9ab8020cd813f6
Daniel Pantea <dani.pantea@gmail.com>
2023-01-23 16:32:46 +0200

    update 23.01.2023

--- src/components/CreateRecipe.js
+++ src/components/CreateRecipe.js
@@ -1,47 +1,70 @@
-import React from 'react';
-// import SearchItem from './SearchItem';
-// import { useSelector } from 'react-redux';
+import React, {useState} from 'react';
+import SearchItem from './SearchItem';
+import { useSelector } from 'react-redux';
 import Ingredient from '../components/Ingredient';
 
 function CreateRecipe({ state }) {
-  // const {filtered} = useSelector((state) => state.ingredients)
+  const { filtered } = useSelector((state) => state.ingredients);
+
+  function CloseAndDiscard() {
+    state(false);
+  }
+
+  const [listOfIngredients, setListOfIngredients] = useState(['Sare', 'Piper'])
 
   return (
-    <div className="h-full bg-gray-200 p-10 text-gray-900">
-      <h3 className="mt-20 text-xl font-semibold">
-        To create a new recipe, please type in all the information below.
-      </h3>
+    <div className="flex flex-col justify-between h-full bg-gray-200 p-4 md:px-10 text-gray-900 min-w-fit">
+      <section className="my-8">
+        <h3 className="mb-8 md:mb-14 text-xl font-semibold">
+          To create a new recipe, please type in all the information below.
+        </h3>
+
+        <label
+          htmlFor="recipe-name"
+          className="block uppercase tracking-wider text-gray-900 font-semibold ml-2 mb-1"
+        >
+          Recipe Name
+        </label>
+        <input
+          className="rounded-2xl w-full lg:w-3/4 xl:w-2/4 h-14 text-xl p-4 bg-blue-300 placeholder:text-gray-600 mb-6"
+          placeholder="Recipe name..."
+          type="text"
+          id="recipe-name"
+          name="recipe-name"
+        />
 
       <label
-        htmlFor="recipe-name"
-        className="block uppercase tracking-wider text-gray-900 font-semibold"
-      >
-        Recipe Name
-      </label>
-      <input
-        className="rounded-2xl h-14 text-xl p-4 bg-blue-300 placeholder:text-gray-600"
-        placeholder="Recipe name..."
-        type="text"
-        id="recipe-name"
-        name="recipe-name"
-      />
+          htmlFor="search-ingredients"
+          className="block uppercase tracking-wider text-gray-900 font-semibold ml-2 mb-1"
+        >
+          Search Ingredients
+        </label>
+        <SearchItem 
+        database={filtered} 
+        setListOfIngredients={setListOfIngredients}
+        />
 
-      {/* <SearchItem 
-        database={filtered}
-      /> */}
+        <div className="list-of-ingredients flex flex-col rounded-2xl bg-gray-100 p-2 mt-6 max-h-60 md:max-h-96 min-w-fit ">
+          <div className="flex flex-col gap-2 overflow-y-auto scrollbar-hide snap-y no-scrollbar">
 
-      <div className="list-of-ingredients">
-        <Ingredient label="Sare" />
-      </div>
+            {listOfIngredients.map((item, index) => 
+      <Ingredient label={item} key={index} />)}
 
-      <div>
+          </div>
+        </div>
+      </section>
+
+      <section className="flex flex-col mt-8 gap-3">
+        <button className="bg-orange-600 p-4 rounded-2xl text-white tracking-widest font-semibold">
+          Save New Recipe
+        </button>
         <button
-          className="bg-blue-200 p-4 rounded-3xl"
-          onClick={() => state(false)}
+          className="bg-blue-200 p-4 rounded-2xl tracking-widest font-semibold"
+          onClick={CloseAndDiscard}
         >
           Discard my changes
         </button>
-      </div>
+      </section>
     </div>
   );
 }
--- src/components/Ingredient.js
+++ src/components/Ingredient.js
@@ -1,19 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { BsFillXCircleFill } from 'react-icons/bs';
 
-function Ingredient({label}) {
+function Ingredient({ label }) {
   return (
-    <div className='flex justify-between bg-blue-300 rounded-2xl p-3 items-center gap-3 h-10'>
-        <div className='flex w-full'>
-
-            <label className='inline-block w-full' htmlFor={label}>{label}</label>
-        </div>
-        <div className='flex gap-2'>
-            <input className='px-2 py-1 w-32 rounded-md outline-0 text-xs' type="number" name={label} id={label} placeholder="adauga cantitatea..." />
-            
-            <button className='px-1 hover:bg-red-400 active:bg-red-500 hover:text-white rounded-sm transition-all shadow-md'>X</button>
-        </div>
+    <div className="flex justify-between bg-blue-300 rounded-2xl p-3 items-center gap-3 snap-start min-w-fit">
+      <div className="flex flex-col w-full">
+        <label className="inline-block" htmlFor={label}>
+          <p className="w-24 overflow-hidden sm:w-full">{label}</p> 
+        </label>
+      </div>
+      <div className="flex items-center">
+        <input
+          className="px-4 py-2 rounded-md outline-0 text-sm mr-1 sm:mr-2 w-32 sm:w-auto bg-gray-100"
+          type="number"
+          name={label}
+          id={label}
+          placeholder="adauga cantitatea..."
+        />
+        <p>g</p>
+        <BsFillXCircleFill
+          className="
+            text-red-700 text-2xl cursor-pointer
+            hover:text-red-500 active:text-red-900 transition-all ml-2 sm:ml-6"
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Ingredient
+export default Ingredient;
