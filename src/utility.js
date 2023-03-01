0e427841ecf719a8b73ca37d6da86fdc102e7ebb
Daniel Pantea <dani.pantea@gmail.com>
2023-03-01 15:45:33 +0200

    update 01.03.2023

--- src/utility.js
+++ src/utility.js
@@ -1,9 +1,8 @@
 function format2Decimals(str) {
-  const num = parseFloat(str);
-  return Math.round(num * 100) / 100;
-
-  // const num = +((str).toFixed(2))
-  // return num
+  // const num = parseFloat(str);
+  // return Math.round(num * 100) / 100;
+  const num = +((+str).toFixed(2))
+  return num
 }
 
 export {format2Decimals}
